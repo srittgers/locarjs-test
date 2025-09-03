db413efdb15fcec77a373d94291214ef6e8c1072
srittgers <steverittgers@gmail.com>
2025-09-03 00:33:18 +0000

    removed some unneeded things

--- script.js
+++
@@ -1,5 +0,0 @@
-console.log("Script loaded!");
-
-// Your WebXR initialization code would go here, for example:
-// navigator.xr?.requestSession('immersive-vr').then(onSessionStarted);
-// function onSessionStarted(session) { /* ... */ }
